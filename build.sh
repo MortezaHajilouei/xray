31a4c1918844cadfd367ae877fd0d3ee44c4caf5
morteza <mortezahajilouei@gmail.com>
2022-11-20 16:18:43 +0330

    (stable)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm ./xray.tar.xz
 go build -o xray -trimpath -ldflags "-s -w -buildid=" ./main
-tar -I 'xz -9 -T0' -cf ./x-ui.tar.xz xray
+tar -I 'xz -9 -T0' -cf ./xray.tar.xz xray geoip.dat geosite.dat LICENSE README.md
 rm xray
